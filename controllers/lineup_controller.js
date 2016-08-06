8ae94e1a02265a974ae23a88f9d35162b351fade
Andrew <andrew.j.heinke@gmail.com>
2016-08-06 11:40:14 -0500

    materialize set up with handlebars

--- controllers/lineup_controller.js
+++ controllers/lineup_controller.js
@@ -5,40 +5,8 @@ var method = require('method-override');
 var bodyParser = require('body-parser');
 
 router.get('/', function(req, res) {
-  res.redirect('/lineup');
+  res.render('index');
 });
 
-router.get('/lineup', function(req, res) {
-  models.burgers.findAll()
-    .then(function(data) {
-      res.render('index', {
-        burgers: data
-      });
-    });
-});
-
-router.post('/lineup/create', function(req, res) {
-  models.burgers.create({
-      burger_name: req.body.name,
-      devoured: 0
-    })
-    .then(function(data) {
-      res.redirect('/lineup');
-    });
-});
-
-router.put('/lineup/update/:id', function(req, res) {
-  var burgerID = req.params.id;
-  models.burgers.update({
-      devoured: true
-    }, {
-      where: {
-        id: burgerID
-      }
-    })
-    .then(function(data) {
-      res.redirect('/burgers');
-    });
-});
 
 module.exports = router;
