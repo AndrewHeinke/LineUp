e7931a6ec5110da062017af074051066516ee630
MikeH <mike123.henry@gmail.com>
2016-08-10 12:40:40 -0500

    added color code of markers according to line length and fixed info window not opening issue

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -3,153 +3,119 @@
 
 var currentURL = window.location.origin;
 $.get(currentURL + "/locations", function(mapData){
+  var map;
+  var bounds = new google.maps.LatLngBounds();
+  //30.298063, -97.785785 middle of lake austin
+  var markersArray = [];
 
-    var map;
-    var bounds = new google.maps.LatLngBounds();
-    //30.298063, -97.785785 middle of lake austin
-    var markersArray = [];
+  // Display a map on the page
+  map = new google.maps.Map(document.getElementById("map"));
 
-    // Display a map on the page
-    map = new google.maps.Map(document.getElementById("map")//, {
-         // center: {lat: 30.298063, lng: -97.785785},
-         // zoom: 6
-        //}
-        );
-    //map.setTilt(45);
-
-var markers = [];
-for (var i = 0; i < mapData.length; i++) {
-    markers.push({location_name:   mapData[i].location_name,
-                               lat:   mapData[i].latitude,
-                               lng:   mapData[i].longitude});
-}
-// for (var i = 0; i < markers.length; i++) {
-//     console.log("markers[ " +i+" ].location_name = " + markers[i].location_name);
-//     console.log("markers[ " +i+" ].lat = " + markers[i].lat);
-//     console.log("markers[ " +i+" ].lng = " + markers[i].lng);
-//     console.log("typeOf lat = " + typeof markers[i].lat)
-// }
-//geolocation//
-var currentLat = 0;
-var currentLng = 0;
-var gotCurrentLocation = false;
-var curPos = {};
-if (navigator.geolocation) {
+  //geolocation//
+  var gotCurrentLocation = false;
+  var curPos = {};
+  var curMarker = new google.maps.Marker({});
+  if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
-                currentLat = position.coords.latitude;
-                currentLng = position.coords.longitud;
-                console.log("position.coords.latitude = "+ position.coords.latitude + "typeof "+ (typeof currentLat));
+      curPos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+      console.log("curPos.lat = "+ curPos.lat );
+      console.log("curPos.lng = "+ curPos.lng)
+      gotCurrentLocation = true;
+      curMarker = new google.maps.Marker({
+        position: curPos,
+        map: map,
+        title: "current location",
+        icon: {
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 4
+        }
+      });
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+      console.log("handleLocationError(true");
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+    console.log("handleLocationError(false,");
+  }
 
-                console.log("position.coords.longitude = "+ position.coords.longitude)
-                var curPos = {
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                    };
-gotCurrentLocation = true;
-                //infoWindow.setPosition(pos);
-                //infoWindow.setContent('Location found.');
-                //map.setCenter(pos);
-                //
-            var curMarker = new google.maps.Marker({
-                     position: curPos,
-                     map: map,
-                     title: "current location",
-                     icon: {
-                        path: google.maps.SymbolPath.CIRCLE,
-                        scale: 2
-                            }
-                 });
-                //
-            }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-            console.log("handleLocationError(true");
+  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
+  }
+  //end geolocation//
+  //Voting location markers
+  var markers = [];
+  for (var i = 0; i < mapData.length; i++) {
+    markers.push({
+      location_name:  mapData[i].location_name,
+      lat:            mapData[i].latitude,
+      lng:            mapData[i].longitude
+    });
+  }
+  // Display multiple markers on a map
+  var infoWindow = new google.maps.InfoWindow();
+  var marker;
+  var i;
+  // Loop through our array of markers & place each one on the map
+  for( i = 0; i < mapData.length; i++ ) {
+    //trap for incorrect lat or lng
+    if (typeof markers[i].lat === "number" || typeof markers[i].lng === "number") {
+      var position = new google.maps.LatLng(markers[i].lat, markers[i].lng);
+      bounds.extend(position);
+      marker = new google.maps.Marker({
+        position:   position,
+        map:        map,
+        title:      markers[i].location_name
+        });
+      markersArray.push(marker);
+      // Allow each marker to have an info window
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          //infoWindow.setContent('We need to put something meaningful here!!!');
+          //infoWindow.setContent(infoWindowContent[i][0]);
+          infoWindow.setContent('<div class="info_content">' +
+          //'<p><a href="'+mapData[i].url+'" target="_blank">' + mapData[i].location_name + '</a></p>'+
+          'We need to put something meaningful here!!!</div>');
+          marker.addListener('click', function() {
+            infoWindow.open(map, marker);
           });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
-          console.log("handleLocationError(false,");
         }
-
-    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
+      })(marker, i));
+      //add code to change the color desired
+      console.log("mapData["+i+"].line_length = "+mapData[i].line_length +" -- "+mapData[i].location_name)
+      if(mapData[i].line_length < 2){
+        //short line is green
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
+      } else if(mapData[i].line_length === 2){
+        //medium line is yellow
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/yellow-dot.png')
+      }else{
+        //long line is red
+        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
       }
-//end geolocation//
-
-     //just incase there are markers already on the map
-        //clear markers
-        clearMarkers(markersArray);
-        // Display multiple markers on a map
-         var infoWindow = new google.maps.InfoWindow();
-         var marker;
-         var i;
-
-     // Loop through our array of markers & place each one on the map
-         for( i = 0; i < mapData.length; i++ ) {
-             //trap for missing lat or lng
-             if (markers[i].lat === null || markers[i].lng === null) {
-            }
-             else{
-                var position = new google.maps.LatLng(markers[i].lat, markers[i].lng);
-                 bounds.extend(position);
-                // if (i === 0) {
-                //     marker = new google.maps.Marker({
-                //      position: position,
-                //      map: map,
-                //      title: markers[i].location_name,
-                //      icon: {
-                //         path: google.maps.SymbolPath.CIRCLE,
-                //         scale: 10
-                //             }
-                //  });
-                // } else {
-                     marker = new google.maps.Marker({
-                         position: position,
-                         map: map,
-                         title: markers[i].location_name
-                     });
-               // }
-                 markersArray.push(marker);
-                 // Allow each marker to have an info window
-                 google.maps.event.addListener(marker, 'click', function(marker, i) {
-                 return function() {
-                         //infoWindow.setContent(infoWindowContent[i][0]);
-                         infoWindow.setContent('<div class="info_content">' +
- //                                            '<p><a href="'+mapData[i].url+'" target="_blank">' + mapData[i].location_name + '</a></p>'+
-                                             'xxxx</div>');
-                         marker.addListener('click', function() {
-                             infoWindow.open(map, marker);
-                         });
-                     }
-                 });
-                 //add code to change the color desired
-                 if(i===0){
-                     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
-                 }
-                 else if(i===1){
-                    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
-                 }
-                 else{
-                     marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
-                 }
-                 // Automatically center the map fitting all markers on the screen
-                 map.fitBounds(bounds);
-             }
-         }
-     });
-
-    function clearMarkers(markersArray){
-        //passed an array with the gogle maps marker objects saved when the markers are created
-        for (var i = 0; i < markersArray.length; i++) {
-            markersArray[i].setMap(null);
-        }
-        markersArray = [];
-        bounds = new google.maps.LatLngBounds();
-     }
-$( window ).resize(function() {
-    console.log("resize ran");
-  map.fitBounds(bounds);
+    // Automatically center the map fitting all markers on the screen
+    map.fitBounds(bounds);
+    }
+  }
 });
+
+    // function clearMarkers(markersArray){
+    //     //passed an array with the gogle maps marker objects saved when the markers are created
+    //     for (var i = 0; i < markersArray.length; i++) {
+    //         markersArray[i].setMap(null);
+    //     }
+    //     markersArray = [];
+    //     bounds = new google.maps.LatLngBounds();
+    //  }
+// $( window ).resize(function() {
+//     console.log("resize ran");
+//   map.fitBounds(bounds);
+// });
 });
