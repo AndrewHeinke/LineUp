4a91705a7fcb3be101124d6784c8de0f88694883
MikeH <mike123.henry@gmail.com>
2016-08-11 12:35:24 -0500

    goefence working! need to add fence coords to database

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -9,10 +9,18 @@ $(document).ready(function() {
 
     // Display a map on the page
     map = new google.maps.Map(document.getElementById("map"));
-
-    //geolocation//
     var gotCurrentLocation = false;
     var curPos = {};
+
+
+
+
+
+
+//geo function
+function getGeoLocation(map, callback){
+      //geolocation//
+
     var curMarker = new google.maps.Marker({});
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
@@ -22,6 +30,8 @@ $(document).ready(function() {
         };
       console.log("xxx curPos.lat = "+ curPos.lat );
       console.log("curPos.lng = "+ curPos.lng);
+      console.log(callback);
+      callback();
       gotCurrentLocation = true;
       curMarker = new google.maps.Marker({
         position: curPos,
@@ -43,12 +53,19 @@ $(document).ready(function() {
     }
 
     function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      console.log("handleLocationError has run")
       infoWindow.setPosition(pos);
       infoWindow.setContent(browserHasGeolocation ?
         'Error: The Geolocation service failed.' :
         'Error: Your browser doesn\'t support geolocation.');
     }
     //end geolocation//
+//callback(map, curPos);
+}
+//end geo function
+//marker function
+getGeoLocation(map, function(){
+//var setMapMarkers = function(map, curPos){
     //Voting location markers
     var markers = [];
     for (var i = 0; i < mapData.length; i++) {
@@ -107,20 +124,44 @@ $(document).ready(function() {
       map.fitBounds(bounds);
       }
     //geofences
-    // var epochCoords = [
-    //     {lat: 30.359470, lng: -97.734654},
-    //     {lat: 30.359314, lng: -97.734728},
-    //     {lat: 30.359157, lng: -97.734451},
-    //     {lat: 30.359436, lng: -97.734286}
-    //   ];
     var epochCoords = [
+        {lat: 30.359470, lng: -97.734654},
+        {lat: 30.359314, lng: -97.734728},
+        {lat: 30.359157, lng: -97.734451},
+        {lat: 30.359436, lng: -97.734286}
+      ];
+    var homeCoords = [
         {lat: 29.836809, lng: -97.763282},
 {lat: 29.835598, lng: -97.765219},
 {lat: 29.833923, lng: -97.763717},
 {lat: 29.835328, lng: -97.761550}
       ];
+      //var thisPlace = [];
+      console.log("curPos.lat = "+curPos.lat);
       var thisPlace = new google.maps.LatLng(curPos.lat, curPos.lng);
-      var epochFence = new google.maps.Polygon({
+      //var thisFence = [];
+      // thisFence.push( new google.maps.Polygon({
+      //   paths: epochCoords,
+      //   strokeColor: '#FF0000',
+      //   strokeOpacity: 0.8,
+      //   strokeWeight: 2,
+      //   fillColor: '#FF0000',
+      //   fillOpacity: 0.35,
+      //   map: map
+      //   });
+      //);
+      // thisFence.push( new google.maps.Circle({
+      //       strokeColor: '#FF0000',
+      //       strokeOpacity: 0.8,
+      //       strokeWeight: 2,
+      //       fillColor: '#FF0000',
+      //       fillOpacity: 0.35,
+      //       map: map,
+      //       center: {lat: mapData[i].latitude,  lng: mapData[i].longitude},
+      //       radius:  50
+      //     })
+      // );
+      var thisFence = new google.maps.Polygon({
         paths: epochCoords,
         strokeColor: '#FF0000',
         strokeOpacity: 0.8,
@@ -128,28 +169,50 @@ $(document).ready(function() {
         fillColor: '#FF0000',
         fillOpacity: 0.35,
         map: map
-      });
-      var resultContains = google.maps.geometry.poly.containsLocation(thisPlace, epochFence);
+        });
+
+var thisOtherFence = new google.maps.Polygon({
+        paths: homeCoords,
+        strokeColor: '#FF0000',
+        strokeOpacity: 0.8,
+        strokeWeight: 2,
+        fillColor: '#FF0000',
+        fillOpacity: 0.35,
+        map: map
+        });
+      //var resultContains = google.maps.geometry.poly.containsLocation(thisPlace[i], thisFence[i]);
+      console.log("thisPlace = "+ thisPlace );
+      console.log("thisFence = "+ thisFence);
+      if (i === 4) {
+        var resultContains = google.maps.geometry.poly.containsLocation(thisPlace, thisOtherFence);
+      } else{
+        var resultContains = google.maps.geometry.poly.containsLocation(thisPlace, thisFence);
+      }
+
       console.log("resultContains = "+resultContains);
 //circles
         //for (var city in citymap) {
           // Add the circle for this city to the map.
-          var cityCircle = new google.maps.Circle({
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.8,
-            strokeWeight: 2,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            map: map,
-            center: {lat: markers[i].lat,  lng: markers[i].lng},
-            radius:  50
-          });
+          // var cityCircle = new google.maps.Circle({
+          //   strokeColor: '#FF0000',
+          //   strokeOpacity: 0.8,
+          //   strokeWeight: 2,
+          //   fillColor: '#FF0000',
+          //   fillOpacity: 0.35,
+          //   map: map,
+          //   center: {lat: markers[i].lat,  lng: markers[i].lng},
+          //   radius:  50
+          // });
 
 
 
 //end circles
     //end geofences
-    }
+      }
+
+    });
+//end marker function
+//getGeoLocation(map, setMapMarkers);
   });
 });
 
