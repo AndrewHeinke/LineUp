50e2779506e42154b7a4fc8c72e5375ff51987d9
Andrew <andrew.j.heinke@gmail.com>
2016-08-12 18:04:28 -0500

    updated handlebars templates

--- server.js
+++ server.js
@@ -46,6 +46,6 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 // routes ======================================================================
 require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
 
-// launch ======================================================================
+// launch
 app.listen(port);
 console.log('The magic happens on port ' + port);
