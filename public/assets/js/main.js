741831991743d27291cec5c76540e8ece7c360e9
MikeH <mike123.henry@gmail.com>
2016-08-11 21:15:21 -0500

    saving main.js again

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,11 +1,14 @@
+var mapObj = {};
 $(document).ready(function() {
-
+  var markersArray = [];
+  var map;
   var currentURL = window.location.origin;
   $.get(currentURL + "/locations", function(mapData){
-    var map;
+    mapObj = mapData;
+
     var bounds = new google.maps.LatLngBounds();
     //30.298063, -97.785785 middle of lake austin
-    var markersArray = [];
+
 
     // Display a map on the page
     map = new google.maps.Map(document.getElementById("map"));
@@ -18,7 +21,7 @@ $(document).ready(function() {
 
 
 //geo function
-function getGeoLocation(map, callback){
+var getGeoLocation =  function(map, callback){
       //geolocation//
 
     var curMarker = new google.maps.Marker({});
@@ -42,10 +45,13 @@ function getGeoLocation(map, callback){
           scale: 4
         }
       });
-    }, function() {
+    },
+    // getCurrentPosition error handler
+    function() {
       handleLocationError(true, infoWindow, map.getCenter());
       console.log("handleLocationError(true");
     });
+      // end getCurrentPosition error handler
     } else {
     // Browser doesn't support Geolocation
     handleLocationError(false, infoWindow, map.getCenter());
@@ -104,31 +110,35 @@ getGeoLocation(map, function(){
             });
           }
         })(marker, i));
-
+var fenceColor = "";
         //add code to change the color desired
         //console.log("mapData["+i+"].line_length = "+mapData[i].line_length +" -- "+mapData[i].location_name)
         if( mapData[i].line_length === null ){
-          //out of time frame line is gray
+          //out of time frame line is blue
           marker.setIcon('http://maps.google.com/mapfiles/ms/icons/blue-dot.png')
+          fenceColor = "#0000FF";
         } else if(mapData[i].line_length < 2){
           //short line is green
           marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png')
+          fenceColor = "#00FF00";
         } else if(mapData[i].line_length === 2){
           //medium line is yellow
           marker.setIcon('http://maps.google.com/mapfiles/ms/icons/yellow-dot.png')
+          fenceColor = "#00FFFF";
         }else{
           //long line is red
           marker.setIcon('http://maps.google.com/mapfiles/ms/icons/red-dot.png')
+          fenceColor = "#FF0000";
         }
       // Automatically center the map fitting all markers on the screen
       map.fitBounds(bounds);
       }
     //geofences
     var fenceCoords = [
-        {lat: mapData[i].latitude + 0.00015, lng: mapData[i].longitude + 0.00015},
-        {lat: mapData[i].latitude - 0.00015, lng: mapData[i].longitude + 0.00015},
-        {lat: mapData[i].latitude - 0.00015, lng: mapData[i].longitude - 0.00015},
-        {lat: mapData[i].latitude + 0.00015, lng: mapData[i].longitude - 0.00015}
+        {lat: mapData[i].latitude + 0.0002, lng: mapData[i].longitude + 0.0002},
+        {lat: mapData[i].latitude - 0.0002, lng: mapData[i].longitude + 0.0002},
+        {lat: mapData[i].latitude - 0.0002, lng: mapData[i].longitude - 0.0002},
+        {lat: mapData[i].latitude + 0.0002, lng: mapData[i].longitude - 0.0002}
       ];
 
       //var thisPlace = [];
@@ -137,8 +147,8 @@ getGeoLocation(map, function(){
 
       var thisFence = new google.maps.Polygon({
         paths: fenceCoords,
-        strokeColor: '#FF0000',
-        strokeOpacity: 0.8,
+        strokeColor: fenceColor,
+        strokeOpacity: 1.0,
         strokeWeight: 2,
         fillColor: '#FF0000',
         fillOpacity: 0.35,
@@ -154,14 +164,79 @@ getGeoLocation(map, function(){
         console.log("You are not close enough to "+ mapData[i].location_name + " to enter line length data");
       }
 
-      //console.log("resultContains = "+resultContains);
+/////////////////////
 
-    //end geofences
-      }
+      google.maps.event.addListener(thisFence, 'click', (function(thisFence, i, resultContains) {
 
-    });
+        return function(event) {
+          var lineData = mapData[i].line_length;
+          if (typeof(lineData) !== "number") {
+            lineData=0;
+          }
+          var contentString = '<p><a href="/location/'+mapData[i].id+'/' +lineData+'/'+resultContains+'">'+mapData[i].location_name+'</a></p>';
+          console.log("contentString = "+contentString)
+  infoWindow.setContent(contentString);
+  infoWindow.setPosition(event.latLng);
+  console.log("asd")
+  console.log(thisFence.latLng)
+ // thisFence.addListener('click', function() {
+              infoWindow.open(map, thisFence);
+            //});
+            //infoWindow.setContent('We need to put something meaningful here!!!');
+            // //infoWindow.setContent(infoWindowContent[i][0]);
+            // infoWindow.setContent('<div class="info_content">' +
+            // //'<p><a href="'+mapData[i].url+'" target="_blank">' + mapData[i].location_name + '</a></p>'+
+            // i +': We need to put something meaningful here!!!</div>');
+            // marker.addListener('click', function() {
+            //   infoWindow.open(map, marker);
+            // });
+          }
+        })(thisFence, i, resultContains));
+
+
+
+///////////////////
+        // Allow each geofence to have an info window
+        // thisFence.addListener('click', showMe);
+
+// function showMe(event) {
+//   console.log($(event).;
+//   // if (typeof(lineData) !== "number" ) {
+//   //   lineData = 0;
+//   // }
+//   alert("clicked")
+//   var contentString = '<a href="/location/'+mapData[i].id+'/' +lineData+'/'+resultContains+'"></a>';
+//   infoWindow.setContent(contentString);
+//   infoWindow.setPosition(event.latLng);
+//   infoWindow.open(map);
+//         // // Since this polygon has only one path, we can call getPath() to return the
+//         // // MVCArray of LatLngs.
+//         // var vertices = this.getPath();
+
+//         // var contentString = '<b>Bermuda Triangle polygon</b><br>' +
+//         //     'Clicked location: <br>' + event.latLng.lat() + ',' + event.latLng.lng() +
+//         //     '<br>';
+
+//         // // Iterate over the vertices.
+//         // for (var i =0; i < vertices.getLength(); i++) {
+//         //   var xy = vertices.getAt(i);
+//         //   contentString += '<br>' + 'Coordinate ' + i + ':<br>' + xy.lat() + ',' +
+//         //       xy.lng();
+//         // }
+
+//         // // Replace the info window's content and position.
+//         // infoWindow.setContent(contentString);
+//         // infoWindow.setPosition(event.latLng);
+
+//         // infoWindow.open(map);
+//       }
+//     //end geofences
+       }
+
+     });
 //end marker function
 //getGeoLocation(map, setMapMarkers);
-  });
+
+   });
 });
 
